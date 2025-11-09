c458a04c1d241dbfcacb08817db82e8e2f64321b
Syed-Ahammad <105214007+Syed-Ahammad@users.noreply.github.com>
2025-11-09 08:58:10 +0400

    input validation

--- README.md
+++ README.md
@@ -15,3 +15,8 @@ bun run index.ts
 This project was created using `bun init` in bun v1.2.21. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
 
 server and client running both together using bun add -d concurrently 3:23 running both apps together chapter
+
+
+
+
+10/10/25 - finished pre-commit checks and husky setup
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,16 @@
 import express, { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
+const client = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -12,6 +19,41 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/api/hello', (req: Request, res: Response) => {
    res.send({ message: 'Hello from the server!' });
 });
+
+const conversations = new Map<string, string>();
+
+const chatSchema = z.object({
+   prompt: z.string()
+   .trim()
+   .min(1, 'Prompt cannot be empty')
+   .max(1000, 'Prompt is too long (max 1000 characters)'),
+   conversationId: z.string().uuid()
+})
+
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      res.status(400).json( parseResult.error.format());
+      return;
+   }
+
+   const { prompt, conversationId } = req.body;
+
+   const response = await client.responses.create({
+      model: 'gpt-4o-mini',
+      input: prompt,
+      temperature: 0.2,
+      max_output_tokens: 100,
+      previous_response_id: conversations.get(conversationId),
+   });
+
+   conversations.set(conversationId, response.id);
+
+   res.json({ message: response.output_text })
+});
+
 
 app.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}`);
